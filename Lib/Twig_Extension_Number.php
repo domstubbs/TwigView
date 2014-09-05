41738ce96d1b62c741a3a1f243d4c45a80e2d8d8
Dom Stubbs <dominic@vaya.ws>
2014-09-05 11:36:00 +0100

    Updated Number extension

--- Lib/Twig_Extension_Number.php
+++ Lib/Twig_Extension_Number.php
@@ -1,92 +1,46 @@
 <?php
 
-App::import('Helper', 'Number');
-
-/**
- * Number Helper Filters
- * 
- * Use: {{ '3535839525'|size }} //=> 3.29 GB
- * Use: {{ '0.555'|p(2) }} //=> 0.56
- * Use: {{ '5999'|curr }} //=> $5,999.00
- * Use: {{ '2.3'|pct }} //=> 2.30%
- * 
- *
- * @author Kjell Bublitz <m3nt0r.de@gmail.com>
- * @package TwigView
- * @subpackage TwigView.Lib
- */
-class Cake_Number_Filters {
-
-/**
- * Wrapper to Number->toReadableSize()
- * 
- * @param integer $length Size in bytes
- */
-	static function size($var) {
-		$number = new NumberHelper();
-		return $number->toReadableSize($var);
-	}
-
-/**
- * Wrapper to Number->toPercentage()
- * 
- * @param float $number A floating point number
- * @param integer $precision The precision of the returned number
- */
-	static function percentage($var, $p = 2) {
-		$number = new NumberHelper();
-		return $number->toPercentage($var, $p);
-	}
-
-/**
- * Wrapper to Number->currency()
- * 
- * @param float $number
- * @param string $currency Valid values are 'USD', 'EUR', 'GBP'
- * @param array $options f.e. 'before' and 'after' options.
- */
-	static function currency($var, $curr='USD', $opts=array()) {
-		$number = new NumberHelper();
-		return $number->currency($var, $curr, $opts);
-	}
-
-/**
- * Wrapper to Number->precision()
- * 
- * @param float $number A floating point number
- * @param integer $precision The precision of the returned number
- */
-	static function precision($var, $p=2) {
-		$number = new NumberHelper();
-		return $number->precision($var, $p);
-	}
-}
+App::uses('CakeNumber', 'Utility');
 
 /**
  * Twig_Extension_Number
- * 
- * Use: {{ '3535839525'|size }} //=> 3.29 GB
- * Use: {{ '0.555'|p(2) }} //=> 0.56
- * Use: {{ '5999'|curr }} //=> $5,999.00
- * Use: {{ '2.3'|pct }} //=> 2.30%
- * 
+ *
+ * Use: {{ '3535839525'|toReadableSize }} //=> 3.29 GB
+ * Use: {{ '0.555'|precision(2) }} //=> 0.56
+ * Use: {{ '5999'|currency }} //=> $5,999.00
+ * Use: {{ '2.3'|toPercentage }} //=> 2.30%
+ *
  * @author Kjell Bublitz <m3nt0r.de@gmail.com>
  * @package TwigView
  * @subpackage TwigView.Lib
  */
 class Twig_Extension_Number extends Twig_Extension {
 
-/**
- * Returns a list of filters to add to the existing list.
- *
- * @return array An array of filters
- */
+	/**
+	 * Get declared filters
+	 *
+	 * @return array Twig_SimpleFilters
+	 */
 	public function getFilters() {
 		return array(
-			'size' => new Twig_Filter_Function('Cake_Number_Filters::size'),
-			'pct'  => new Twig_Filter_Function('Cake_Number_Filters::percentage'),
-			'curr' => new Twig_Filter_Function('Cake_Number_Filters::currency'),
-			'p'    => new Twig_Filter_Function('Cake_Number_Filters::precision'),
+			new Twig_SimpleFilter('precision', 'CakeNumber::precision'),
+			new Twig_SimpleFilter('toReadableSize', 'CakeNumber::toReadableSize'),
+			new Twig_SimpleFilter('fromReadableSize', 'CakeNumber::fromReadableSize'),
+			new Twig_SimpleFilter('toPercentage', 'CakeNumber::toPercentage'),
+			new Twig_SimpleFilter('format', 'CakeNumber::format'),
+			new Twig_SimpleFilter('formatDelta', 'CakeNumber::formatDelta'),
+			new Twig_SimpleFilter('currency', 'CakeNumber::currency'),
+		);
+	}
+
+	/**
+	 * Get declared functions
+	 *
+	 * @return array Twig_SimpleFunctions
+	 */
+	public function getFunctions() {
+		return array(
+			new Twig_SimpleFunction('defaultCurrency', 'CakeNumber::defaultCurrency')
 		);
 	}
 
