bc023cd93ce05a0f369f0abf9084062b19b82c31
Dom Stubbs <dominic@vaya.ws>
2014-11-10 12:01:57 +0000

    Removed inadvertently imported shell template docblock

--- Console/Command/TwigViewShell.php
+++ Console/Command/TwigViewShell.php
@@ -1,14 +1,4 @@
 <?php
-/**
- * Copyright 2009 - 2014, Cake Development Corporation (http://cakedc.com)
- *
- * Licensed under The MIT License
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright Copyright 2009 - 2014, Cake Development Corporation (http://cakedc.com)
- * @license MIT License (http://www.opensource.org/licenses/mit-license.php)
- */
-
 App::uses('Shell', 'Console');
 App::uses('AppShell', 'Console/Command');
 App::uses('Folder', 'Utility');
